ad9671680c79429f41a94c9360e9efdfcc75ae7d
Feruzbek <allayorovferuzbek707@gmail.com>
2021-11-07 23:38:30 +0500

    Js test

---
+++ script.js
@@ -0,0 +1,19 @@
+var nameUser = prompt('Write your name');
+var userAge = +prompt('How old are you?');
+var agreeUser = prompt('You should solve some Math problems');
+var a = prompt('6 + 3 =');
+var b = prompt('14 - 3 =');
+var c = prompt('7 * 9 =');
+var d = prompt('17 % 4 =');
+var finish =alert('finished');
+
+
+
+console.log(' my name is ' + nameUser + '!'); 
+console.log(' i am ' + userAge + ' year~s old ');
+console.log(agreeUser);
+console.log(' 6 + 3 = ' + (6 + 3), ' your answer is ' + a);
+console.log(' 14 - 3 = ' + (14 - 3), ' your answer is ' + b);
+console.log(' 7 * 9 = ' + (7 * 9), ' your answer is ' + c);
+console.log(' 17 % 4 =' + (17 % 4), 'your answer is ' + d);
+
